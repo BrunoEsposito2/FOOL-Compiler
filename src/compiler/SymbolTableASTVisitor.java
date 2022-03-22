40a843691915d94fd5a276ab415bd50201b7cee6
Filippo Cavallari <filippo.cavallari99@gmail.com>
2022-03-22 13:04:50 +0100

    Add visit methods for the new oop nodes

--- src/compiler/SymbolTableASTVisitor.java
+++ src/compiler/SymbolTableASTVisitor.java
@@ -10,6 +10,8 @@ import static compiler.lib.FOOLlib.nlJoin;
 public class SymbolTableASTVisitor extends BaseASTVisitor<Void,VoidException> {
 	
 	private List<Map<String, STentry>> symTable = new ArrayList<>();
+	private Map<String, Map<String, STentry>> classTable = new HashMap<>();
+	private HashSet<String> localDeclaration;
 	private int nestingLevel=0; // current nesting level
 	private int decOffset=-2; // counter for offset of local declarations at current nesting level 
 	int stErrors=0;
@@ -226,7 +228,154 @@ public class SymbolTableASTVisitor extends BaseASTVisitor<Void,VoidException> {
 		return null;
 	}
 
+	@Override
+	public Void visitNode(FieldNode fieldNode) {
+		return super.visitNode(fieldNode);
+	}
 
+	@Override
+	public Void visitNode(MethodNode methodNode) {
+		if (print) printNode(methodNode);
 
+		var symbolTable = symTable.get(nestingLevel);
+		var parameters = new ArrayList<TypeNode>();
+		methodNode.parlist.stream().map(DecNode::getType).forEach(parameters::add);
+		var methodType = new MethodTypeNode(new ArrowTypeNode(parameters, methodNode.retType));
+		methodNode.setType(methodType);
 
+		nestingLevel += 1;
+		var hmn = new HashMap<String, STentry>();
+		symTable.add(hmn);
+		var previousNestingLevelDecOffset = decOffset;
+		int parametersOffset=1;
+		for(var parNode : methodNode.parlist){
+			if(hmn.put(parNode.id, new STentry(nestingLevel, parNode.getType(), parametersOffset++)) != null){
+				System.out.format("Parameter %s at line %d has already been declared!", parNode.id, parNode.getLine());
+				stErrors++;
+			}
+		}
+		methodNode.declist.forEach(this::visit);
+		visit(methodNode.exp);
+
+		symTable.remove(nestingLevel--);
+		decOffset = previousNestingLevelDecOffset;
+
+		return null;
+	}
+
+	@Override
+	public Void visitNode(ClassNode classNode) {
+		if (print) printNode(classNode);
+		var symbolTable = symTable.get(0);
+		localDeclaration = new HashSet<>();
+		var type = new ClassTypeNode(new ArrayList<>(), new ArrayList<>());
+		var virtualTable = new HashMap<String, STentry>();
+
+		classNode.type = type;
+		var entry = new STentry(0, type, decOffset--);
+		if (symbolTable.put(classNode.id, entry) != null) {
+			System.out.format("Class %s at line %d has already been declared!", classNode.id, classNode.getLine());
+			stErrors += 1;
+		}
+
+		classTable.put(classNode.id, virtualTable);
+		symTable.add(virtualTable);
+
+		nestingLevel += 1;
+		//Fields handling
+		var fieldOffset = -(type.allFields.size()) - 1;
+		for(var field : classNode.fields) {
+			if(localDeclaration.contains(field.id)){
+				System.out.format("Field %s at line %d has already been declared!", field.id, field.getLine());
+				stErrors += 1;
+			}else{
+				localDeclaration.add(field.id);
+				field.offset = fieldOffset;
+				virtualTable.put(field.id, new STentry(nestingLevel, field.getType(), field.offset));
+				fieldOffset -= 1;
+				type.allFields.add(field.getType());
+			}
+		}
+
+		var previousNestingLevelDecOffset = decOffset;
+		decOffset = type.allMethods.size();
+
+		//Methods handling
+		for(var method : classNode.methods){
+			if(localDeclaration.contains(method.id)){
+				System.out.format("Method %s at line %d has already been declared!", method.id, method.getLine());
+				stErrors += 1;
+			}else{
+				localDeclaration.add(method.id);
+				visit(method);
+				virtualTable.put(method.id, new STentry(nestingLevel, method.getType(), decOffset));
+				decOffset += 1;
+				type.allMethods.add(((MethodTypeNode)method.getType()).fun);
+			}
+		}
+		symTable.remove(nestingLevel--);
+		decOffset = previousNestingLevelDecOffset;
+		return null;
+	}
+
+	@Override
+	public Void visitNode(ClassCallNode classCallNode) {
+		if (print) printNode(classCallNode);
+
+		var objectEntry = stLookup(classCallNode.objectId);
+		if(objectEntry == null){
+			System.out.format("Object %s at line %d has not been declared!", classCallNode.objectId, classCallNode.getLine());
+			stErrors += 1;
+		}else{
+			//Fetch the method
+			if (!(objectEntry.type instanceof RefTypeNode)){
+				System.out.format("Object %s at line %d has not a RefTypeNode!", classCallNode.objectId, classCallNode.getLine());
+				stErrors += 1;
+			} else{
+				String classId = ((RefTypeNode) objectEntry.type).id;
+				STentry methodEntry = classTable.get(classId).get(classCallNode.methodId);
+				if (methodEntry == null){
+					System.out.format("Method %s at line %d has not been declared!", classCallNode.methodId, classCallNode.getLine());
+					stErrors += 1;
+				}else{
+					classCallNode.entry = objectEntry;
+					classCallNode.methodEntry = methodEntry;
+					classCallNode.nl = nestingLevel;
+				}
+			}
+		}
+		classCallNode.arglist.forEach(this::visit);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(EmptyNode n) {
+		if (print) printNode(n);
+
+		return null;
+	}
+
+	@Override
+	public Void visitNode(NewNode newNode) {
+		if(print) printNode(newNode);
+
+		//Checks if ID is in ClassTable (class has to be declared)
+		if(!(classTable.containsKey(newNode.classId))){
+			System.out.format("Class %s at line %d has not been declared!", newNode.classId, newNode.getLine());
+			stErrors += 1;
+		}else{
+			var classEntry = symTable.get(0).get(newNode.classId);
+			//Class needs to be declared at level 0, otherwise it is invalid
+			if(classTable == null){
+				System.out.format("Class %s at line %d has not been declared at level 0!", newNode.classId, newNode.getLine());
+				stErrors += 1;
+			}else{
+				newNode.entry = classEntry;
+			}
+		}
+
+		newNode.arglist.forEach(this::visit);
+
+		return null;
+	}
 }
